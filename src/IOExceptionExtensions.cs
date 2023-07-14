7e16ba3949b8576e0cb80dfbe91c7281a651dcb3
Victor Nova <lostfreeman@gmail.com>
2023-07-14 15:34:18 -0700

    handle File already exists on Linux

--- src/IOExceptionExtensions.cs
+++ src/IOExceptionExtensions.cs
@@ -1,8 +1,13 @@
 ﻿namespace PIDFile;
 
 using System.IO;
+using System.Runtime.InteropServices;
 
 static class IOExceptionExtensions {
+    const int LINUX_FILE_ALREADY_EXISTS = 17;
+
     public static bool IsFileAlreadyExists(this IOException error)
-        => error.HResult == unchecked((int)0x80070050);
+        => RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+            ? error.HResult == unchecked((int)0x80070050)
+            : error.HResult == LINUX_FILE_ALREADY_EXISTS;
 }
